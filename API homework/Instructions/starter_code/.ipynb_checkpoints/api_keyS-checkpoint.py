504aee7d7911389cf4c06d73f95a549cd7c440d1
Stella Schubert <stellaschubert@Stellas-MacBook-Air.local>
2021-12-25 16:25:39 +1100

    API key working

---
+++ API homework/Instructions/starter_code/.ipynb_checkpoints/api_keyS-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "47c5fd10904d3eb96a2816cb25394cbe"
+
+# Google API Key
+g_key = "AIzaSyCASWm3ov9FQhP3tr25JC3NAMXH2wXiKQ0"
--- API homework/Instructions/starter_code/api_keys.py
+++ API homework/Instructions/starter_code/api_keys.py
@@ -1,5 +1,11 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = 'a66d6e9094774bfc52e92ab2184187bb'
+
+#Weather. Api: a66d6e9094774bfc52e92ab2184187bb
+
+
+
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+#g_key = "AIzaSyCASWm3ov9FQhP3tr25JC3NAMXH2wXiKQ0"
+
